c18639df7f58ad2fbb3342a9475add773d1b8a0d
Cole Davenport <25422844+vampartyslayer@users.noreply.github.com>
2024-07-13 17:10:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -666,7 +666,6 @@ const CONTRACT_ABI = [
 	}
 ];
 
-
 const BASE_SEPOLIA_CHAIN_ID = '84532'; // Chain ID for Base Sepolia
 const BASE_SEPOLIA_PARAMS = {
     chainId: '0x14CC4', // Chain ID in hex (84532 in decimal)
